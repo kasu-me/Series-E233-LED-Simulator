6753e12d25c568d172f030fdd947a57aaf7f28c5
ahozura <66002768+kasu-me@users.noreply.github.com>
2025-09-25 22:28:57 +0900

    SNSカード：画像枚数の定数化と画像生成アルゴリズムの一部修正

--- resources/img/card.php
+++ resources/img/card.php
@@ -18,7 +18,11 @@
 	$overlayImageFiles=[];
 	$paramIndex=0;
 	$firstParamKey="";
-	
+
+	define("MAKU_COUNT_PER_IMAGE_IKISAKI", 100);
+	define("MAKU_COUNT_PER_IMAGE_SHUBETSU_SMALL", 28);
+	define("MAKU_COUNT_PER_IMAGE_SHUBETSU_LARGE", 100);
+
 	foreach ($_GET as $key => $value) {
 		$imgId=$key;
 		if(($key!="shu"&&$key!="iki"&&$paramIndex==0) || $key=="shu"){
@@ -41,22 +45,12 @@
 			}
 			//小さい種別画像
 			else{
-				if($imgId<28){
-					$img2 = imagecreatefrompng("../led/ledtypeS000.png");
-				}else{
-		 			$img2 = imagecreatefrompng("../led/ledtypeS".str_pad(floor(($imgId-28)/29)+1,3,0,STR_PAD_LEFT).".png");
-				}
+				$img2 = imagecreatefrompng("../led/ledtypeS".str_pad(floor($imgId/MAKU_COUNT_PER_IMAGE_SHUBETSU_SMALL)+1,3,0,STR_PAD_LEFT).".png");
 		 		// 合成する画像のサイズを取得
 				$sx = imagesx($img2);
 				$sy = imagesy($img2);
 				imageLayerEffect($img, IMG_EFFECT_ALPHABLEND);// 合成する際、透過を考慮する
-				if($imgId<28){
-					imagecopyresized($img, $img2, 109, 32, 51*$imgId, 0, 144, 96, 48, 32); // 合成する
-				}else if($imgId<56){
-					imagecopyresized($img, $img2, 109, 32, 51*($imgId-28), 0, 144, 96, 48, 32 ); // 合成する
-				}else{
-					imagecopyresized($img, $img2, 109, 32, 51*(($imgId-56)), 0, 144, 96, 48, 32 ); // 合成する
-				}
+				imagecopyresized($img, $img2, 109, 32, (51*$imgId)-($imgId%MAKU_COUNT_PER_IMAGE_SHUBETSU_SMALL), 0, 144, 96, 48, 32); // 合成する
 				imagedestroy($img2); // 破棄
 			}
 		}else if(($key!="shu"&&$key!="iki"&&$paramIndex==1) || $key=="iki"){
@@ -68,12 +62,12 @@
 			if($key=="iki"){
 				$imgId=$value;
 			}
-	 		$img2 = imagecreatefrompng("../led/ledimgA".str_pad(floor($imgId/100),3,0,STR_PAD_LEFT).".png");
+	 		$img2 = imagecreatefrompng("../led/ledimgA".str_pad(floor($imgId/MAKU_COUNT_PER_IMAGE_IKISAKI),3,0,STR_PAD_LEFT).".png");
 	 		// 合成する画像のサイズを取得
 			$sx = imagesx($img2);
 			$sy = imagesy($img2);
 			imageLayerEffect($img, IMG_EFFECT_ALPHABLEND);// 合成する際、透過を考慮する
-			imagecopyresized($img, $img2, 252, -99*($imgId%100)+32, 0, 0, $sx*3, $sy*3, $sx, $sy); // 合成する
+			imagecopyresized($img, $img2, 252, -99*($imgId%MAKU_COUNT_PER_IMAGE_IKISAKI)+32, 0, 0, $sx*3, $sy*3, $sx, $sy); // 合成する
 			imagedestroy($img2); // 破棄
 		}else if($imgId=="col"){
 			//色の場合は後で処理するためここでは何もしない
